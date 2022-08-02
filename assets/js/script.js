ad8803ed3dd6b2cab742b893139c16a1e0eb9571
lausanchez98 <lauusancheez@gmail.com>
2022-08-01 20:04:39 -0500

    Se agrega el archivo index.html asi como los archivos de JavaScript revisados en clase.

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+
+//Este es un comentario de una sola linea usando dos diagonales 
+
+/*Este es un
+comentario multilinea usando
+una diagonal y asterisco para abrir 
+y asterisco y diagonal para cerrar*/
+
+//Una alerta es una ventana de dialogo que muestra un mensaje
+// alert ("Hola Laura, estoy vive!");
+
+//Pedimos la informacion al usuario con un prompt, la guarda en una variable llamada nombre para despues mostrarla
+nombre = prompt("¿Como te llamas?");
+
+//Usamos un console.log para mostrar lo que se guardo en la variable nombre
+console.log(nombre);
+
+//JavaScript es case sensitive
+
+
+//CamelCase
+
+/*UpperCamelCase - ComidaFavoritaDeFelipe 
+lowerCamelCase  - comidaFavoritaDeFelipe*/
+
+
+//Programa para calcular el area de un tringulo
+
+base = prompt("Ingrese la base del triangulo");
+altura = prompt("Ingrese la altura del triangulo");
+areaTriangulo = (base*altura)/2;
+alert(areaTriangulo);
+//Concatenacion del texto mas el resultado
+console.log("El area del triangulo es:"+areaTriangulo);
---
+++ assets/js/variables.js
@@ -0,0 +1,56 @@
+//Mejores practicas
+
+//CamelCase
+// Utilizar mayusculas en la mitad de nuestras variables
+// variableTriangulo, miNombre, miEdad, miDireccion
+
+//Palabras reservadas 
+// No usar palabras reservadas para declarar una variable
+// if, return, true, false, for, while, let, va, etc...
+
+// var while;
+// var null ;
+// var if;
+// var true;
+// var false;
+// var var;
+// var const;
+// var let;
+
+//Seguir un estandar personal
+
+//Pueden contener numeros, letras, guion bajo, simbolo de dolar
+var a;
+var a1;
+var a_Nombre;
+var a$Direccion;
+
+//Declaracion de variables
+// Type number
+var edad = 24;
+var peso = 78;
+var miopia = 3;
+var astigmatismo = 3;
+
+// Type Boolean
+var ciego = true;
+var hambre = true;
+var bateria = false;
+
+//Type String
+var nombre = "Laura Sanchez";
+var dir = 'Monterrey';
+var nacionalidad = `Mexicana`;
+
+//Variables declaradas con un mismo valor
+var x, y, z = 4;
+
+//Variables undefined
+var xx, yy, zz;
+
+// Variable tres = 4
+// var dos = tres
+// var uno = tres
+var uno = dos = tres = 4;
+
+//Escribir en una sola linea variables de nuestro carrito de compras 
