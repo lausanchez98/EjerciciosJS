61ce591a6fa5703019cbc479a0764cc5aee58663
lausanchez98 <lauusancheez@gmail.com>
2022-08-02 22:54:24 -0500

    Se agrega ejercicio 1 de JS desarrollado en clase.

---
+++ assets/js/practica1.js
@@ -0,0 +1,35 @@
+// Boolean variables
+var tenerSueño = true;
+var respirar = true;
+var estaSuscrito = true;
+var inATeam = true;
+var libroLeido = true;
+var talent = false;
+var aproboMateria = false;
+var existeEspacio = false; 
+var isPositive = false;
+var willToLive= false;
+
+// String variables
+var frase = "Cada día sabemos más y entendemos menos.";
+var nombreMascota= "Lilo";
+var proverb = "Actions speak louder than words";
+var mensaje = "Hola, gracias por visitar!";
+var matricula= "1658895";
+var zipCode= "75765";
+var tweet = "I hate mondays :(";
+var apellido = "Sanchez";
+var placa = "RNB-543";
+var address = "31898 Gottlieb Brooks Suite 164 - Twin Falls, CT / 47827";
+
+// Numeric variables
+var estatura = 166;
+var edad = 24;
+var songsInPlaylist = 400;
+var weight = 68;
+var añosExperiencia = 7;
+var puntajeExamen = 80;
+var resultado = 14;
+var moviesWatched = 431;
+var temperatura = 34;
+var numeroDeMascotas = 4;
