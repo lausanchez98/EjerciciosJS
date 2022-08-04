09a9f60995ae042188e6ac146914b5360daeb742
lausanchez98 <lauusancheez@gmail.com>
2022-08-03 21:51:43 -0500

    Se añade archivo condicional.js donde se estudia el comportamiento de las sentencias de control de flujo if y switch en JS.

---
+++ assets/js/condicional.js
@@ -0,0 +1,143 @@
+// const dinero = false;
+// const hambre = true;
+
+//alert(dinero == hambre); // false
+//alert(dinero || hambre); // true
+
+//AND
+// Todas las condiciones deben ser true
+// true && true  => true
+// true  && false => false
+// false && false => false
+
+//OR
+// Solo una condicion debe ser true
+// true || true = true
+// true || false = true
+// false || false = false
+
+//NOT
+// Es la negacion a la condicion
+// true => false, false => true
+
+// var hora = 8;
+
+// if (hora == 8) {
+// 	alert("Buen dia");
+// }
+
+// var hora = 3;
+
+// if (hora != 8) {
+// 	console.log("Buen día");
+// }
+
+// var hungry = false;
+
+// if (hungry == true) {
+// 	alert("Tengo mal del porky");
+// } else {
+
+// 	alert("Two mimir");
+// }
+
+// var hour = 20;
+
+// if (hour < 18) {
+//  alert("Good day");
+// } else {
+//   alert("Tardes, ya");
+// }
+
+// var hour = 19;
+
+// if (hour <= 18) {
+//  alert("Todavía hay sol");
+// } else {
+//   alert("Tardes, ya");
+// }
+
+// var edad = 21;
+
+// if (edad >= 18) {
+// 	alert("Crear cuenta");
+// } else {
+// 	alert("bai bai");
+// }
+
+// var hora = 12;
+
+// if (hora <= 7 && hora > 0) {
+// 	alert("Buenas madrugadas");
+// } else {
+// 	alert("Que tenga un buen dia");
+// }
+
+// var hora = 12;
+
+// if (hora > 0) {
+
+// 	if (hora <= 7) {
+//     	alert("Buenas madrugadas");
+//     } else {
+//     	alert("Que tenga un buen dia");
+//     }
+
+// } else {
+
+// 	alert("Su hora no existe");
+
+// }
+
+// var hora = 12;
+
+// // Verificar  24 >= hora > 0
+
+// if (hora > 0 && hora <= 24) {
+// 	// Codigo
+
+//     if (hora < 7) {
+//     	alert("Buenas madrugadas");
+//     } else {
+//     	alert("Que tenga un buen dia");
+//     }
+
+// } else {
+// 	alert("Su hora no es válida");
+// }
+
+// Switch
+
+// Obtener la fecha actual
+
+// var day = new Date().getDay();
+
+// function evalDate(day) {
+//   switch (day) {
+//     case 0:
+//       return "Domingo";
+//       break;
+//     case 1:
+//       return "Lunes";
+//       break;
+//     case 2:
+//       return "Martes";
+//       break;
+//     case 3:
+//       return "Miercoles";
+//       break;
+//     case 4:
+//       return "Jueves";
+//       break;
+//     case 5:
+//       return "Viernes";
+//       break;
+//     case 6:
+//       return "Sabado";
+//       break;
+//     default:
+//       return "Su fecha no es valida";
+//   }
+// }
+
+// alert(evalDate(0));
